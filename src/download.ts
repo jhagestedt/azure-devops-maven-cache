0b998b388a6581c4e2c264621592894050f3f9f3
Julien Hagestedt <julien.hagestedt@gmail.com>
2019-06-16 07:06:57 +0200

    Azure DevOps task

---
+++ src/download.ts
@@ -0,0 +1,38 @@
+import tl = require('azure-pipelines-task-lib');
+
+async function run() {
+  try {
+    const storage_account: string = tl.getInput('storage_account', true);
+    if (storage_account == 'bad') {
+      tl.setResult(tl.TaskResult.Failed, 'Bad input for Azure storage account.');
+      return;
+    }
+    const storage_key: string = tl.getInput('storage_key', true);
+    if (storage_key == 'bad') {
+      tl.setResult(tl.TaskResult.Failed, 'Bad input for Azure storage key.');
+      return;
+    }
+    const storage_container: string = tl.getInput('storage_container', true);
+    if (storage_container == 'bad') {
+      tl.setResult(tl.TaskResult.Failed, 'Bad input for Azure storage container.');
+      return;
+    }
+    tl.execSync("az", "storage container create"
+      + " --account-name " + storage_account
+      + " --account-key " + storage_key
+      + " --name " + storage_container);
+    tl.execSync("az", "storage blob download"
+      + " --account-name " + storage_account
+      + " --account-key " + storage_key
+      + " --container-name " + storage_container
+      + " --name repository"
+      + " --file repository.zip");
+    tl.execSync("mkdir", "-p ~/.m2");
+    tl.execSync("unzip", "repository.zip -d ~/.m2");
+    tl.execSync("rm", "-rf repository.zip");
+  } catch (err) {
+    tl.setResult(tl.TaskResult.Failed, err.message);
+  }
+}
+
+run();
